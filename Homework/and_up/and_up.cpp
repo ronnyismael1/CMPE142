74e75436b6a442f7fb96a1bfc5d7599ca4cc2bce
Ronny Ismael <ronny.ismael@sjsu.edu>
2023-08-25 09:23:08 -0700

    More work on code

--- Homework/and_up/and_up.cpp
+++ Homework/and_up/and_up.cpp
@@ -1,16 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_WIDTH 16    // Max amounts of bits for visualizing
+
+// Temporary function the help visualize binary bits
+void printBinaryWithZeros(unsigned int number, int width) {
+    for (int i = width - 1; i >= 0; i--) {
+        printf("%d", (number >> i) & 1);
+        if (i % 4 == 0) {
+            printf(" ");
+        }
+    }
+}
+
 int main (int argc, char **argv) {
+    // Checks if we provided any arguments
     if (argc < 2) {
         printf("USAGE: ./and_up numbers_to_and ...");
         exit (1);
     }
-    for (int i = 1; i < argc; i++) {
+    unsigned int result;    // Unsigned to handle binary better
+    char *p;                                          // Initialize result to be first arg
+    result = strtol(argv[1], &p, 10);                 // ""
+    printf("%u\t", result);                           // ""
+    printBinaryWithZeros(result, MAX_WIDTH);          // ""
+    printf("\n");                                     // ""
+
+    // For loop to perform bitwise AND on arguments
+    for (int i = 2; i < argc; i++) {
         char *p;
         long conv = strtol(argv[i], &p, 10);
+        result &= conv;
 
-        printf("%ld\n", conv);
+        printf("%ld\t", conv);
+        printBinaryWithZeros(conv, MAX_WIDTH);
+        printf("\n");
     }
+
+    // Prints results below
+    printf("%ld\t", result);
+    printBinaryWithZeros(result, MAX_WIDTH);
+    printf("\n");
     exit(0);
 }
+
+// Temporary function to help convert decimal to binary
+void printBinary(unsigned int number) {
+    if (number > 1) {
+        printBinary(number / 2);
+    }
+    printf("%d", number % 2);
+};
