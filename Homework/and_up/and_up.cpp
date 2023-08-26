2b8ba2c6987efd8a108c46b2745e6d88991d4a96
Ronny Ismael <ronny.ismael@sjsu.edu>
2023-08-25 19:57:16 -0700

    Changing cpp file to have the binary test

--- Homework/and_up/and_up.cpp
+++ Homework/and_up/and_up.cpp
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cctype>
+
+#define MAX_WIDTH 16    // Max amounts of bits for visualizing
+
+// Temporary function the help visualize binary bits
+void printBinary(unsigned int number, int width);
 
 int main (int argc, char **argv) {
     // Checks if we provided any arguments
@@ -12,19 +18,36 @@ int main (int argc, char **argv) {
     // For loop to perform bitwise AND on arguments
     for (int i = 1; i < argc; i++) {
         long conv = strtol(argv[i], &p, 0); // Base 0 to auto-detect if it is hex, dec, or octal
-        // If it is not a number
         if (*p) { // If p is pointing to non-null then it means argv[i] wasn't fully a number
-            printf("%s is not a number\n", argv[i]);
+            //printf("%s is not a number\n", argv[i]);
+            unsigned int temp = 0xFFFF;
             for (int j = 0; argv[i][j] != '\0'; j++) {
                 conv = argv[i][j];
                 result &= conv;
+                printf("%c\t", argv[i][j]);
+                printBinary(conv, MAX_WIDTH);
+                printf("\n");
             }
-        // If it is a decimal, hex, or octal
         }else {
             result &= conv;
+            printf("%ld\t", conv);
+            printBinary(conv, MAX_WIDTH);
+            printf("\n");
         }
     }
     // Prints results below
-    printf("%ld", result);
+    printf("----------------------------\n%ld\t", result);
+    printBinary(result, MAX_WIDTH);
+    printf("\n");
     exit(0);
+}
+
+// Temporary function
+void printBinary(unsigned int number, int width) {
+    for (int i = width - 1; i >= 0; i--) {
+        printf("%d", (number >> i) & 1);
+        if (i % 4 == 0) {
+            printf(" ");
+        }
+    }
 }
