fd9f29654483ec8656b3dc3240d1959b78b283e7
Ronny Ismael <ronny.ismael@sjsu.edu>
2023-09-08 11:06:56 -0700

    fixed blank

--- Homework/biased_bits/biased_bits.c
+++ Homework/biased_bits/biased_bits.c
@@ -59,7 +59,7 @@ int main(int argc, char *argv[]) {
         int set_bits = countSetBits(num);
         size_t line_length = strlen(line)-1;
         // Check if it is not integer, too long, or line is blank
-        if (*endptr != '\0' && *endptr != '\n' || line_length > 16 || *line == '\n') {
+        if (*endptr != '\0' && *endptr != '\n' || line_length > 16 || read == 1 && *line == '\n') {
             printf("invalid: %s", line);
             clean(fp, line, next_line);
             exit(2);
